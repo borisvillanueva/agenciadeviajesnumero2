c98c19cbfea6b5482c7193e4dfb2da9bf443418c
Boris Villanueva <borisvillanueva@yahoo.com>
2023-05-02 21:25:54 -0600

    initial

--- config/db.js
+++ config/db.js
@@ -6,6 +6,8 @@ const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.D
     host: process.env.DB_HOST,
     port: '3306',
     dialect: 'mysql',
+    // Increase the connection timeout (in milliseconds)
+    connectionTimeout: 10000, // 10 seconds
     define: {
         timestamps: false
     },
@@ -16,6 +18,7 @@ const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.D
         idle: 10000
     },
     operatorAliases: false
+
 });
 
 export default db;
